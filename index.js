7923205aca438bcb89efb6df1e233fa07df9cc41
wang.andy32@gmail.com <wang.andy32@gmail.com>
2018-08-09 20:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var katzDeli = [];
 function takeANumber(katzDeliLine, newName){
-  katzDeli.concat(katzDeliLine);
-  katzDeli.push(newName);
+  if(typeof newName === 'string'){
+    katzDeliLine.push(newName);
+  }else if(typeof newName === "Object")
   return "Welcome, " + newName + ". You are number " + (katzDeli.length) + " in line.";
 }
 
