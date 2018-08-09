e3eb43d57623e5aefb99b269383b320e0218d1d8
wang.andy32@gmail.com <wang.andy32@gmail.com>
2018-08-09 20:35:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var katzDeli = [];
+function takeANumber(katzDeliLine, newName){
+  return "Welcome," + newName + ". You are number" + (katzDeliLine.length +1) + "in line."
+}
+
+function nowServing(){
+  var first = katzDeliLine.shift();
+  return first;
+}
